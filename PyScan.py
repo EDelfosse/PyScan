57704bc7ae7df6037b25ec1358c20243106019b7
EDelfosse <Delfosse57@gmail.com>
2018-08-10 11:40:05 -0500

    Added a nmap scan on detected ports to gain more accuracy

--- PyScan.py
+++ PyScan.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import urllib
 import portscan
 import sys
@@ -14,6 +15,7 @@ parser.add_argument("RHOST",help="The host you would like to scan",type=str)
 parser.add_argument("-F","--fast",help="Scan only the top 100 most used ports. Defualt is top 1,000", action="store_true")
 parser.add_argument("-d","--dictionary",help="Dictionary to bruteforce url filepaths if webserver found",type=str)
 parser.add_argument("-t","--threads",help="Amound of threads to use in scanning, Default 4",default=4,type=int)
+parser.add_argument("--nmap",help="Whether to run an nmap scan to detect service versions",action="store_true")
 args = parser.parse_args()
 
 #Start port scan
@@ -24,8 +26,15 @@ ports = portscan.scan(args.fast,args.threads,args.RHOST)
 for i in range(0,len(ports)):
 	print("Port: %d Open!" % ports[i])
 
-#Checks to see if webserver at any ports, if so then bruteforce URL
-print("\n[+] Starting webserver detection and bruteforce\n")
-httpenum.scan(args.RHOST,ports,args.threads,args.dictionary)
+if len(ports) == 0:
+	print("[+] NO PORTS OPEN. IS THE HOST DOWN?")
+else:
+	#Runs nmap against open ports
+	print("\n[+] Starting nmap port scan against detected ports")
+	os.system("nmap " + args.RHOST + " -sC -sV -p  " + str(ports)[1:-1].strip().replace(" ",""))
+	
+	#Checks to see if webserver at any ports, if so then bruteforce URL
+	print("\n[+] Starting webserver detection and bruteforce\n")
+	httpenum.scan(args.RHOST,ports,args.threads,args.dictionary)
 
-print("\n[+] Scan done")
+	print("\n[+] Scan done")
